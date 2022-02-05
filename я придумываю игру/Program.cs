7b0242854454f8c2ee5b3994a4268a66879edd2d
Y4rman Gaming <yarosvetbobr@gmail.com>
2022-02-05 23:15:14 +0300

    Создание класса программы — «Program»

--- я придумываю игру/Program.cs
+++ я придумываю игру/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Field1;
+
+namespace GameNuel
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Field field = new Field();
+        }
+    }
+}
